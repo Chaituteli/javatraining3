b9f64cffcf08af6cf8d0d4067793a57c981dff65
Chaituteli <102845795+Chaituteli@users.noreply.github.com>
2022-05-19 19:02:44 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,61 @@
+package com.vstl.javatraining;
+
+public class SwitchStatement {
+static int intmonth=4;
+	
+	public  void monthsInyear(int intmonth) {
+
+	
+		switch(intmonth) {
+		
+			
+		case 1:
+			System.out.println("1st month is january");
+			break;
+			
+		case 2:
+			System.out.println("2nd month is february");
+			break;
+			
+		case 3:
+			System.out.println("3rd month is march");
+			break;
+			
+		case 4:
+			System.out.println("4th month is april");
+			break;
+		case 5:
+			System.out.println("5th month is may");
+			break;
+		case 6:
+			System.out.println("6th month is june");
+			break;
+		case 7:
+			System.out.println("7th month is july");
+			break;
+		case 8:
+			System.out.println("8th month is August");
+			break;
+		case 9:
+			System.out.println("9th month is september");
+			break;
+		case 10:
+			System.out.println("1oth month is october");
+			break;
+		case 11:
+			System.out.println("11th month is  november");
+			break;
+		case 12:
+			System.out.println("12th month is december");
+			break;
+		  default:System.out.println("Invalid Month");  
+	    }
+		
+		}
+
+	
+		
+	}
+	
+
+
